0e1ed42045dd9f5ffae7c574e521c593c0e79631
JulianW <julian@wagenschuetz.com>
2020-06-21 15:43:18 +0200

    Add arg to set filemode (w or a)
    - w (write): writes a new file for every log
    - a (append): appends all logs to the same file

--- dask_log_server.py
+++ dask_log_server.py
@@ -6,13 +6,14 @@ import pytz
 import pathlib
 
 
-def dask_logger_config(time_interval=60, log_path="logs/", n_tasks_min=1):
+def dask_logger_config(time_interval=60, log_path="logs/", n_tasks_min=1, filemode="a"):
     def dask_logger(dask_client):
         pathlib.Path(log_path).mkdir(parents=True, exist_ok=True)
 
         def logger():
             thread = threading.currentThread()
             last_time = time.time()
+            datetime_stamp = datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
             while getattr(thread, "do_run", True):
                 if dask_client.status == "running":
                     now_time = time.time()
@@ -27,7 +28,9 @@ def dask_logger_config(time_interval=60, log_path="logs/", n_tasks_min=1):
                             "client_id": str(id(dask_client)),
                             "tasks": tasks
                         }
-                        with open(f"{log_path}/logger_{datetime.datetime.now()}.jsonl", "a") as file:
+                        if filemode == "w":
+                            datetime_stamp = datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
+                        with open(f"{log_path}/logger_{datetime_stamp}.jsonl", filemode) as file:
                             file.write(json.dumps(log_message))
                             file.write("\n")
                 time.sleep(time_interval)
