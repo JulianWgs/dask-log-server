835a40bac3e29478d9196c88d798d1fca23b4223
JulianW <julian@wagenschuetz.com>
2020-07-08 20:34:59 +0200

    Add tasks table

--- app.py
+++ app.py
@@ -22,37 +22,61 @@ external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 df = pd.read_parquet(os.path.join(log_path, "graph.parquet"))
+df_tasks = pd.read_parquet(os.path.join(log_path, "tasks.parquet"))
 
 app.layout = html.Div([
     html.Div([
+        dash_table.DataTable(
+            id='graph-table',
+            columns=[{"name": i, "id": i} for i in df.columns],
+            data=df.to_dict('records'),
+            editable=True,
+            filter_action="native",
+            sort_action="native",
+            sort_mode="multi",
+            column_selectable=False,
+            row_selectable="single",
+            row_deletable=False,
+            selected_columns=[],
+            selected_rows=[],
+            page_action="native",
+            page_current=0,
+            page_size=10,
+        ),
+    ]),
+    html.Div([
         html.Img(
             id="graph",
-            height=300)
-    ]),
-    dash_table.DataTable(
-        id='table',
-        columns=[{"name": i, "id": i} for i in df.columns],
-        data=df.to_dict('records'),
-        editable=True,
-        filter_action="native",
-        sort_action="native",
-        sort_mode="multi",
-        column_selectable=False,
-        row_selectable="single",
-        row_deletable=False,
-        selected_columns=[],
-        selected_rows=[],
-        page_action="native",
-        page_current=0,
-        page_size=10,
-    )
-
+            height=300),
+    ], className="row"),
+    html.Div([
+        dash_table.DataTable(
+            id='tasks-table',
+            columns=[{"name": item, "id": item} for item in [
+                'action', 'start', 'stop', 'worker', 'status', 'nbytes',
+                'thread', 'type', 'typename', 'key', 'datetime',
+                'client_id', 'id', 'duration',
+            ]],
+            editable=True,
+            filter_action="native",
+            sort_action="native",
+            sort_mode="multi",
+            column_selectable=False,
+            row_selectable="single",
+            row_deletable=False,
+            selected_columns=[],
+            selected_rows=[],
+            page_action="native",
+            page_current=0,
+            page_size=10,
+        )
+    ], className="row"),
 ])
 
 
 @app.callback(
     Output(component_id='graph', component_property='src'),
-    [Input(component_id='table', component_property='selected_rows')]
+    [Input(component_id='graph-table', component_property='selected_rows')]
 )
 def load_graph(selected_rows):
     if not selected_rows:
@@ -64,6 +88,17 @@ def load_graph(selected_rows):
     data = dask.dot.to_graphviz(dsk).pipe(format="png")
     encoded_image = base64.b64encode(data).decode()
     return 'data:image/png;base64,{}'.format(encoded_image)
+
+
+@app.callback(
+    Output(component_id='tasks-table', component_property='data'),
+    [Input(component_id='graph-table', component_property='selected_rows')]
+)
+def task_of_graph(selected_rows):
+    if not selected_rows:
+        return list()
+    graph_id = df.iloc[selected_rows].index[0]
+    return df_tasks[df_tasks["id"] == graph_id].to_dict("records")
 
 
 if __name__ == '__main__':
