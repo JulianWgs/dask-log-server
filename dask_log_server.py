542d8abe2b14cae239f8e18f4861d8a769e8a0d8
JulianW <julian@wagenschuetz.com>
2020-08-02 19:52:04 +0200

    Add function to calculate actual concurrency
    from task stream

--- dask_log_server.py
+++ dask_log_server.py
@@ -449,6 +449,31 @@ def _read_dask_graphs(log_path, unique_graph_ids):
     return dask_graphs
 
 
+def get_actual_concurrency(df_tasks):
+    """
+    Calculate worker concurrency of task stream.
+
+    Note: concurrency_sum should have two data points per timestamp with a
+    vertical line connecting them. This is not yet implemented.
+
+    Another Note: Melt puts value columns after one another.
+    """
+    df_concurrency = (
+        df_tasks[(df_tasks["action"] == "compute")]
+        .melt(
+            id_vars=["client_id"], value_vars=["start", "stop"], value_name="datetime"
+        )
+        .set_index("datetime")
+    )
+    df_concurrency["concurrency"] = df_concurrency["variable"].map(
+        {"start": 1, "stop": -1}
+    )
+    df_concurrency["concurrency_sum"] = df_concurrency.groupby("client_id")[
+        "concurrency"
+    ].cumsum()
+    return df_concurrency
+
+
 def get_max_concurrency(log_path, unique_graph_ids):
     """Get maximum theoretical concurrency of task graphs."""
     return (
