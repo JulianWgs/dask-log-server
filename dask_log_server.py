594eadddf9f5e2b86e87a2bfa6a1ed99c1ef262a
JulianW <julian@wagenschuetz.com>
2020-08-14 18:36:54 +0200

    Improve color NaN handling, filter empty attrs

--- dask_log_server.py
+++ dask_log_server.py
@@ -866,7 +866,7 @@ def _get_dsk_attributes(dsk, df_tasks, label_col="", color_col="", current_time=
         random.seed(10)
         unique_colors = {
             value: f"#{random.randint(0, 0xFFFFFF):06X}"
-            for value in df_tasks[color_col].unique()
+            for value in df_tasks[color_col].dropna().unique()
         }
     else:
         raise ValueError("Could not get type on how to color graph.")
@@ -892,6 +892,7 @@ def _get_dsk_attributes(dsk, df_tasks, label_col="", color_col="", current_time=
                 attributes[attribute_name][key]["fillcolor"] = "red"
         elif color_type == "float":
             try:
+                # Minimum value is 0
                 grayscale = 100 - int(df_single_task[color_col] / max_color_value * 100)
             except ValueError:
                 continue
@@ -900,15 +901,15 @@ def _get_dsk_attributes(dsk, df_tasks, label_col="", color_col="", current_time=
             if grayscale < 20:
                 attributes[attribute_name][key]["fontcolor"] = "white"
         elif color_type == "category":
-            attributes[attribute_name][key]["style"] = "filled"
-            attributes[attribute_name][key]["fillcolor"] = unique_colors[
-                df_single_task[color_col]
-            ]
+            color = unique_colors.get(df_single_task[color_col])
+            if color is not None:
+                attributes[attribute_name][key]["style"] = "filled"
+                attributes[attribute_name][key]["fillcolor"] = color
 
     attributes["data"] = {
-        _to_tuple(key): value for key, value in attributes["data"].items()
+        _to_tuple(key): value for key, value in attributes["data"].items() if value
     }
     attributes["func"] = {
-        _to_tuple(key): value for key, value in attributes["func"].items()
+        _to_tuple(key): value for key, value in attributes["func"].items() if value
     }
     return attributes
